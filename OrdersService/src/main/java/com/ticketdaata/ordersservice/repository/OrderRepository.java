63c5ab9c97b4fd4744f7b9b3f7f1d22cfb18498f
niazbuoy08 <rahmanniaz29@gmail.com>
2025-09-20 13:24:08 +0600

    Order Service Updates

--- OrdersService/src/main/java/com/ticketdaata/ordersservice/repository/OrderRepository.java
+++ OrdersService/src/main/java/com/ticketdaata/ordersservice/repository/OrderRepository.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-
-@Repository
 public interface OrderRepository extends MongoRepository<Order, String> {
     
     // Find orders by user ID
